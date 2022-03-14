621cd6ba8fc64ad3ac0abefae2ab7e9f9dfbf5b5
Arif Widayana <arifwidayana@gmail.com>
2022-03-14 04:13:50 -0700

    initial commit

---
+++ app/src/main/java/id/co/binaracademy/chapter2/tr.kt
@@ -0,0 +1,4 @@
+package id.co.binaracademy.chapter2
+
+class tr {
+}
--- app/src/main/java/id/co/binaracademy/chapter2/training1/MainMenu.kt
+++
@@ -1,8 +0,0 @@
-package id.co.binaracademy.chapter2.training1
-
-fun main(){
-    val kelilingSegitiga: Calculate = KSegitiga(3)
-    val luasSegitiga: Calculate = LSegitiga(9,7)
-    println(kelilingSegitiga)
-    println(luasSegitiga)
-}
